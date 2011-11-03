a02906d8c601ff29732bcc6f5942b99c0b8c34af
Dean Mao <deanmao@gmail.com>
2011-11-03 16:16:02 -0700

    get merge working with basics

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
+from django.utils import simplejson
+from google.appengine.api import memcache
+from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import util, template
+import logging
 
 def _request(url, cache_ttl=3600, force=False):
     request_cache_key = 'request:%s' % url
@@ -34,22 +24,17 @@ def _request(url, cache_ttl=3600, force=False):
     return resp
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
-
-class ContentHandler(webapp.RequestHandler):
     def get(self, page):
         skip_cache = self.request.get('cache') == '0'
         try:
-            page = _request('https://shdh.pbworks.com/api_v2/op/GetPage/page/%s' % page, force=skip_cache)
-            if page['folder'] != 'Website':
-                raise LookupError()
+            if page:
+                page = _request('https://hackerdojo.pbworks.com/api_v2/op/GetPage/page/%s' % page, force=skip_cache)
             self.response.out.write(template.render('templates/content.html', locals()))
         except LookupError:
             self.error(404)
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler), ('/(.+)', ContentHandler)],
+    application = webapp.WSGIApplication([('/(.*)', MainHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
 
