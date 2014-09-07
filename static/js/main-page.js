e003faa0acf90283bdb2e4b9df199ae22afd54b9
Martin Schumann <martinschumann@Martins-MacBook.local>
2014-09-06 22:11:31 -0700

    Hackerdojo.css: gave both spans a size and added overflow-y base.html:  moved some scripts that are only loaded by the main page into main-page.js main_page.html:  added main-page.js to extrahead

---
+++ static/js/main-page.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    //$(".right-span").load("http://events.hackerdojo.com #primary");
+    var test = true;
+    if (test == true){
+        $(".join").hide();
+    }
+    $( ".close_button" ).click(function(e) {
+        e.preventDefault();
+        $(".join").hide();
+    });
+    $(window).scroll(function() {
+        var scrollPosition = window.pageYOffset;
+        var windowSize     = window.innerHeight;
+        var bodyHeight     = document.body.offsetHeight;
+        if ($(window).scrollTop() > 50) { // > 100px from top - show div
+            $(".join").addClass('visible');
+        }
+        else { // <= 100px from top - hide div
+            $(".join").removeClass('visible');
+        }
+        if ((Math.max(bodyHeight - (scrollPosition + windowSize), 0) < 10)) { // scroll from bottom - hides to show footer
+            $(".join").removeClass('visible');
+        }
+    });
+});
