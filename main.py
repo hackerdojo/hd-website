aec38e555ac9bdd1e1a30b7742061f815febc7cf
Celine Bursztein <contact@celine.im>
2017-10-31 14:23:15 -0700

    Genie widget on website

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
 from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
@@ -10,6 +6,9 @@ from google.appengine.ext.webapp import template
 import urllib
 import re
 import json
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 PB_WIKI = 'dojowebsite'
 PB_API_URL = 'https://%s.pbworks.com/api_v2/op/GetPage/page/%s'
@@ -21,6 +20,7 @@ LOCAL_TZ = 'America/Los_Angeles'
 if os.environ['SERVER_SOFTWARE'].startswith('Dev'):
     CACHE_ENABLED = False
     CDN_ENABLED = False
+
 
 def _request(url, cache_ttl=3600, force=False):
     request_cache_key = 'request:%s' % url
@@ -36,12 +36,13 @@ def _request(url, cache_ttl=3600, force=False):
             else:
                 resp = json.loads(data.content)
             memcache.set(request_cache_key, resp, cache_ttl)
-            memcache.set(failure_cache_key, resp, cache_ttl*10)
+            memcache.set(failure_cache_key, resp, cache_ttl * 10)
         except (ValueError, urlfetch.DownloadError), e:
             resp = memcache.get(failure_cache_key)
             if not resp:
                 resp = {}
     return resp
+
 
 class PBWebHookHandler(webapp.RequestHandler):
     def post(self):
@@ -54,12 +55,14 @@ class PBWebHookHandler(webapp.RequestHandler):
             memcache.delete(failure_cache_key)
         self.response.out.write("200 OK")
 
+
 class IndexHandler(webapp.RequestHandler):
     def get(self):
         version = os.environ['CURRENT_VERSION_ID']
         if CDN_ENABLED:
             cdn = CDN_HOSTNAME
         self.response.out.write(template.render('templates/index.html', locals()))
+
 
 class StaffHandler(webapp.RequestHandler):
     def get(self):
@@ -69,22 +72,23 @@ class StaffHandler(webapp.RequestHandler):
             cdn = CDN_HOSTNAME
         self.response.out.write(template.render('templates/event_staff.html', locals()))
 
+
 class MainHandler(webapp.RequestHandler):
-    def get(self, pagename, site = PB_WIKI):
+    def get(self, pagename, site=PB_WIKI):
         skip_cache = self.request.get('cache') == '0'
         version = os.environ['CURRENT_VERSION_ID']
         shouldRedirect = False
 
         redirect_urls = {
-          # From: To
-          'give': 'Give',
-          'auction': 'Auction',
-          'Assemble': 'Give',
-          'Mobile Device Lab': 'MobileDeviceLab',
-          'kickstarter': 'http://www.kickstarter.com/projects/384590180/an-events-space-and-a-design-studio-for-hacker-doj',
-          'Kickstarter': 'http://www.kickstarter.com/projects/384590180/an-events-space-and-a-design-studio-for-hacker-doj',
-          'KICKSTARTER': 'http://www.kickstarter.com/projects/384590180/an-events-space-and-a-design-studio-for-hacker-doj',
-          'key': 'http://signup.hackerdojo.com/key',
+            # From: To
+            'give': 'Give',
+            'auction': 'Auction',
+            'Assemble': 'Give',
+            'Mobile Device Lab': 'MobileDeviceLab',
+            'kickstarter': 'http://www.kickstarter.com/projects/384590180/an-events-space-and-a-design-studio-for-hacker-doj',
+            'Kickstarter': 'http://www.kickstarter.com/projects/384590180/an-events-space-and-a-design-studio-for-hacker-doj',
+            'KICKSTARTER': 'http://www.kickstarter.com/projects/384590180/an-events-space-and-a-design-studio-for-hacker-doj',
+            'key': 'http://signup.hackerdojo.com/key',
         }
         if pagename in redirect_urls:
             url = redirect_urls[pagename]
@@ -94,37 +98,36 @@ class MainHandler(webapp.RequestHandler):
                 cdn = CDN_HOSTNAME
             try:
                 pageKey = 'page:%s' % pagename.lower()
-                if not(pagename):
+                if not (pagename):
                     pagename = 'FrontPage'
                 page = _request(PB_API_URL % (site, pagename), cache_ttl=604800, force=skip_cache)
                 # fetch a page where a lowercase version may exist
-                if not(page and "name" in page):
-                  if memcache.get(pageKey):
-                    pagename = memcache.get(pageKey)
-                    page = _request(PB_API_URL % (site, pagename), cache_ttl=604800, force=skip_cache)
-                    shouldRedirect = True
+                if not (page and "name" in page):
+                    if memcache.get(pageKey):
+                        pagename = memcache.get(pageKey)
+                        page = _request(PB_API_URL % (site, pagename), cache_ttl=604800, force=skip_cache)
+                        shouldRedirect = True
                 # Convert quasi-camel-case to spaced words
                 title = re.sub('([A-Z])([A-Z][a-z])', r'\1 \2', pagename)
                 title = re.sub('([a-z])([A-Z])', r'\1 \2', title)
                 if page and "name" in page:
-                  fiveDays = 432000
-                  memcache.set(pageKey, pagename, fiveDays)
-                  if shouldRedirect:
-                    self.redirect(pagename, permanent=True)
-                  else:
-                    version = os.environ['CURRENT_VERSION_ID']
-                    if CDN_ENABLED:
-                        cdn = CDN_HOSTNAME
-                    self.response.out.write(template.render('templates/content.html', locals()))
+                    fiveDays = 432000
+                    memcache.set(pageKey, pagename, fiveDays)
+                    if shouldRedirect:
+                        self.redirect(pagename, permanent=True)
+                    else:
+                        version = os.environ['CURRENT_VERSION_ID']
+                        if CDN_ENABLED:
+                            cdn = CDN_HOSTNAME
+                        self.response.out.write(template.render('templates/content.html', locals()))
                 else:
-                  raise LookupError
+                    raise LookupError
             except LookupError:
                 version = os.environ['CURRENT_VERSION_ID']
                 if CDN_ENABLED:
                     cdn = CDN_HOSTNAME
                 self.response.out.write(template.render('templates/404.html', locals()))
                 self.response.set_status(404)
-
 
 
 app = webapp.WSGIApplication([
